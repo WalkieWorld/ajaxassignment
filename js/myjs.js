8eb23de8193edbab8a79c2dba6f433f0d7408f65
Hao Zhang <WalkieWorld@users.noreply.github.com>
2015-02-23 01:06:06 -0700

    (1)Update style (2)Create myjs.js file

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,21 @@
 /**
  * Created by micer on 2/22/15.
  */
+var myAJAXSearch = {
+
+    inputHandler: document.getElementById("searchArea"),
+
+    init: function(){
+        this.addEvent(this.inputHandler, "keyup", function(e){
+            if(e.keyCode === 13){
+                //
+            }
+        });
+    },
+
+    addEvent: function(handler, event, func){
+        handler.addEventListener(event, func);
+    }
+};
+
+myAJAXSearch.init();
