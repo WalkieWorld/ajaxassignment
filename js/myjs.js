a734ea374aa66e34b782e16a0844afadf58f74d1
Hao Zhang <WalkieWorld@users.noreply.github.com>
2015-02-25 07:36:38 -0700

    Update a function which shows results

--- js/myjs.js
+++ js/myjs.js
@@ -7,6 +7,8 @@ var myAJAXSearch = {
     acctKey: "AhLlAkkOGyJW6bPqjpbuyJzQaFODbaDxrKwKilID9yE",
     servOption: ["/Web", "/Image", "/News", "/Video"],
     searchSentence: undefined,
+    resultObj: undefined,
+    renderHtml: undefined,
     option: {
         method: "GET",
         url: "https://api.datamarket.azure.com/Bing/Search/v1",
@@ -16,9 +18,12 @@ var myAJAXSearch = {
     init: function(){
         this.addEvent(this.inputHandler, "keyup", function(e){
             if(e.keyCode === 13){
+                if(myAJAXSearch.renderHtml !== undefined){
+                    document.getElementById("result").removeChild(myAJAXSearch.renderHtml);
+                }
                 myAJAXSearch.searchSentence = encodeURIComponent("'" + document.getElementById("searchArea").value + "'");
                 myAJAXSearch.option.url += myAJAXSearch.servOption[0]
-                + "?$format=json&Query=" + myAJAXSearch.searchSentence;
+                + "?$format=json&$top=15&Query=" + myAJAXSearch.searchSentence;
                 myAJAXSearch.ayncAJAX(myAJAXSearch.option, myAJAXSearch.ayncCallback);
             }
         });
@@ -48,10 +53,31 @@ var myAJAXSearch = {
     ayncCallback: function(request){
         var result = document.getElementById("result");
         if(request.status === 200){
-            result.textContent = request.responseText;
+            myAJAXSearch.resultObj = JSON.parse(request.responseText);
+            myAJAXSearch.renderResult(myAJAXSearch.resultObj);
         }else{
             result.textContent = request.statusText;
         }
+    },
+
+    renderResult: function(jsonData){
+        var section = document.createElement("section");
+        var ul = document.createElement("ul");
+        jsonData.d.results.forEach(function(curVal, index, arr){
+            var li = document.createElement("li");
+            var a = document.createElement("a");
+            a.href = curVal.Url;
+            a.textContent = curVal.Title;
+            a.target="_blank";
+            var p = document.createElement("p");
+            p.textContent = curVal.Description;
+            li.appendChild(a);
+            li.appendChild(p);
+            ul.appendChild(li);
+        });
+        section.appendChild(ul);
+        myAJAXSearch.renderHtml = section;
+        document.getElementById("result").appendChild(myAJAXSearch.renderHtml);
     }
 };
 
